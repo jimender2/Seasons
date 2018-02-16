3baf39f1540be94b86230e60ddea208587a0ea1c
BuildTools <jmeredit@ashland.edu>
2018-02-16 10:28:11 -0500

    Added cases

--- src/Seasons.java
+++ src/Seasons.java
@@ -14,8 +14,13 @@ public class Seasons {
 				System.out.println(month + " is in Winter ");
 				break;
 			case "APRIL": case "MAY": case "JUNE":
+				System.out.println(month + " is in Spring");
+				break;
+			case "JULY": case "AUGUST": case "SEPTEMBER":
 				System.out.println(month + " is in Summer");
 				break;
+			case "OCTOBER": case "NOVEMBER": case "DECEMBER":
+				System.out.println(month + " is in Fall");
 			default:
 				System.out.println("Month is not valid");
 		}
