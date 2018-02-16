107aeb5bbf6200e07822bb211f0510ee15bcb11e
BuildTools <jmeredit@ashland.edu>
2018-02-15 22:42:53 -0500

    Master

--- src/Seasons.java
+++ src/Seasons.java
@@ -1,4 +1,19 @@
+import java.util.*;
+import java.util.Scanner;
+
 
 public class Seasons {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String month;
+		month = scan.nextLine();
+		month = month.toUpperCase();
+		switch (month){
+			case "JANUARY": case "Febuary":
+				System.out.println("Month is ");
+				break;
+			default:
+				System.out.println("Month is not valid");
+		}
+	}
 }
