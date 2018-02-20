d7859c93a6e8fcaddab9a706340672b22cbebacb
BuildTools <jmeredit@ashland.edu>
2018-02-19 22:34:49 -0500

    Final

--- src/Seasons.java
+++ src/Seasons.java
@@ -2,24 +2,31 @@ import java.util.*;
 import java.util.Scanner;
 /*
 Jonathan Meredith
-This program does something that I really dont remember.
+This program gets input from the user about what month it is.
+From there the program tells them what season the month is in.
 */
 	
 
 public class Seasons {
 	public static void main(String[] args) {
+		
+		//Set up scanner
 		Scanner scan = new Scanner(System.in);
-		String month;
+		
+		//Declare Variables
+		String month,
+				upperCaseMonth;
+		
+		//Get input from the user
+		System.out.println("Please enter the month:");
 		month = scan.nextLine();
 		scan.close();
-		month = month.toUpperCase();
 		
-		String firstLetter = month.char(0).toUpperCase();
-	    String restLetters = data.substring(1).toLowerCase();
-	      return firstLetter + restLetters;
-	      
-	      
-		switch (month){
+		//Convert month to all uppercase to test the different cases
+		upperCaseMonth = month.toUpperCase();
+		
+		//Check what season the month is in and display the result
+		switch (upperCaseMonth){
 			case "DECEMBER": case "JANUARY": case "FEBURARY":
 				System.out.println(month + " is in Winter ");
 				break;
@@ -31,6 +38,7 @@ public class Seasons {
 				break;
 			case "SEPTEMBER": case "OCTOBER": case "NOVEMBER":
 				System.out.println(month + " is in Fall");
+				break;
 			default:
 				System.out.println("Month is not valid");
 		}
